b9e6880e1729af3edb5d1efa0b18202f8e72b0ce
konojunya <junya03dance@gmail.com>
2018-02-19 07:09:19 +0900

    たぶんできた

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"image"
+	"image/color"
 	_ "image/jpeg"
 	"log"
 	"os"
@@ -10,68 +11,47 @@ import (
 	"github.com/nfnt/resize"
 )
 
+type DominantColor struct {
+	Red   uint8
+	Green uint8
+	Blue  uint8
+}
+
 func main() {
 	file, err := os.Open("sample.jpg")
 	defer file.Close()
 	if err != nil {
-		log.Fatal(err)
-		return
+		log.Fatalf("open err: %v", err)
 	}
 	img, _, err := image.Decode(file)
 	if err != nil {
-		log.Fatal(err)
-		return
+		log.Fatalf("decode err: %v", err)
 	}
 
 	img = resize.Resize(36, 0, img, resize.Lanczos3)
-	hist := getHistgram(img)
+	rgba := getDominantColor(img)
 
-	max := 0
-
-	for _, val := range hist {
-		if val > max {
-			max = val
-		}
-	}
-	r, g, b := int2rgb(max)
-	colorCode := "#" + dec2hex(r, 2) + dec2hex(g, 2) + dec2hex(b, 2)
-
-	fmt.Println(colorCode)
+	fmt.Println(rgba)
 
 }
 
-func getHistgram(img image.Image) []int {
-	hist := make([]int, 1000000)
+func getDominantColor(img image.Image) DominantColor {
+	var r, g, b, count float64
 
 	rect := img.Bounds()
 	for i := 0; i < rect.Max.Y; i++ {
 		for j := 0; j < rect.Max.X; j++ {
-			r, g, b, _ := img.At(j, i).RGBA()
-			i := rgb2int(int(r), int(g), int(b))
-			hist[i]++
+			c := color.RGBAModel.Convert(img.At(j, i))
+			r += float64(c.(color.RGBA).R)
+			g += float64(c.(color.RGBA).G)
+			b += float64(c.(color.RGBA).B)
+			count++
 		}
 	}
 
-	return hist
-}
-
-func dec2hex(n, beam int) string {
-	hex := ""
-	str := "0123456789abcdef"
-	for i := 0; i < beam; i++ {
-		m := n & 0xf
-		hex = string(str[m]) + hex
-		n -= m
-		n >>= 4
+	return DominantColor{
+		Red:   uint8(r / count),
+		Green: uint8(g / count),
+		Blue:  uint8(b / count),
 	}
-
-	return hex
-}
-
-func rgb2int(r, g, b int) int {
-	return (((r >> 5) << 6) | ((g >> 5) << 3) | ((b >> 5) << 0))
-}
-
-func int2rgb(i int) (r, g, b int) {
-	return ((i >> 6 & 0x7) << 5) + 16, ((i >> 3 & 0x7) << 5) + 16, ((i >> 0 & 0x7) << 5) + 16
 }
